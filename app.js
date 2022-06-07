abc836cab971777cd3bcd793bdf227f88dc0e541
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2022-06-07 00:32:20 -0400

    adding unplash api

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { get } from "https"; // native node modole for calling requrest
 import bodyParser from "body-parser";
 import 'dotenv/config' 
 
-// import fetch from "node-fetch";
+import fetch from "node-fetch";
 
 const app = express();
 
@@ -23,8 +23,22 @@ let obj = {
     humidity:0,
     wind:0,
     feelslike:"",
-    imageURL:""
+    imageURL:"",
+    backgroundURL:" "
 }
+
+async function catchImage(){
+    const query = obj.cityName
+    const API_ID = process.env.UNPLASH_API;
+    const unplashURL = 'https://api.unsplash.com/photos/random?orientation=landscape&query='
+    + query +'&count=1&client_id=' + API_ID;
+    const response = await fetch(unplashURL);
+    const jsondata = await response.json();
+    obj.backgroundURL= jsondata[0].urls.regular
+}
+
+
+catchImage()
 
 app.get("/", function(req, res){
     res.render("weather",{
@@ -34,7 +48,8 @@ app.get("/", function(req, res){
         humidity: obj.humidity,
         wind: obj.wind,
         feelslike: obj.feelslike,
-        imageURL : obj.imageURL
+        imageURL : obj.imageURL,
+        backgroundURL: obj.backgroundURL
     })
     
 })
@@ -42,6 +57,8 @@ app.get("/", function(req, res){
 app.post("/",function(req,res){
     let query = req.body.cityName;
     obj.cityName = query;
+    catchImage(query);
+    console.log(obj)
     const apiKEY = process.env.WEATHER_API;
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKEY +"&units=" + unit;
@@ -54,16 +71,12 @@ app.post("/",function(req,res){
             obj.humidity = weatherData.main.humidity;
             obj.feelslike = weatherData.main.feels_like;
             obj.imageURL = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"
+            console.log(obj);
 
             res.redirect("/");
-            // res.write("<h1>The temperatur in " + query+ " is " + temp + " degree Celcius</h1>");
-            // res.write("<p> The weather is currently " + weatherDescription + "</p>")
-            // res.write("<img src=" +imageURL +">");
-            // res.send();
+         
         })
     })
-
-
 })
 
 app.listen(3000,function(){
